da4f0550427591db644ae280c75848b4c3b46ca4
Soori <soori.vakilpour@hotmail.com>
2024-04-06 17:21:41 +0100

    3rd practice code didnt give an array, exit code always seems zero

---
+++ Floyds_Aglorithm_3rd.py
@@ -0,0 +1,20 @@
+import sys
+import itertools
+
+NO_PATH = sys.maxsize
+graph = [[1, 7, NO_PATH, 8], [NO_PATH, 0, 5, NO_PATH], [NO_PATH, NO_PATH, NO_PATH, 0]]
+MAX_LENGTH = len(graph[0])
+
+def floyd(distance):
+    for intermediate, start_node, end_node in itertools.product(range(MAX_LENGTH), range(MAX_LENGTH), range(MAX_LENGTH)):
+        if start_node == end_node:
+            distance[start_node][end_node] = 0
+            continue
+        distance[start_node][end_node] = min(distance[start_node][end_node],
+                                             distance[start_node][intermediate] + distance[intermediate][end_node])
+
+        shortest_distance = floyd(distance)
+        print(shortest_distance)
+
+
+
